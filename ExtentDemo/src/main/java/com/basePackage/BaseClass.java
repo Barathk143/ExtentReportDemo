1a4d98285731989c550e7c2b6a8e84edeff7bf59
Barathk143 <96520923+Barathk143@users.noreply.github.com>
2022-01-12 16:32:24 +0530

    Update BaseClass.java

--- ExtentDemo/src/main/java/com/basePackage/BaseClass.java
+++ ExtentDemo/src/main/java/com/basePackage/BaseClass.java
@@ -43,6 +43,7 @@ public class BaseClass {
 		chromeOptions.addArguments("headless");
 		chromeOptions.addArguments("window-size=1980,1080");
 	    driver = new ChromeDriver(chromeOptions);
+		chromeOptions.setBinary("/Chromedriver.exe");
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/index.php/");
 	}
